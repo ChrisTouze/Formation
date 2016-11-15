4fdffbea8c8c66ef9830558dd77a1202eec27b24
ChrisTouze <admin@lr-services.fr>
2016-11-15 13:35:06 +0100

    UPDATE message
    commentaire

--- app/src/main/java/com/example/chris/holdall/MainActivity.java
+++ app/src/main/java/com/example/chris/holdall/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.chris.holdall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button);
+        TextView textView = (TextView)findViewById(R.id.my_text);
+        textView.setText("Bonjour");
     }
 }
