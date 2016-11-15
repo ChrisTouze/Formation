47bbe8163bd5cdb45c15110bbcbf065e040b07cd
ChrisTouze <admin@lr-services.fr>
2016-11-15 15:55:32 +0100

    ADD new activity

--- app/src/main/java/com/example/chris/holdall/MainActivity.java
+++ app/src/main/java/com/example/chris/holdall/MainActivity.java
@@ -15,14 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button test1 = (Button) findViewById(R.id.test1);
+        Button test2 = (Button) findViewById(R.id.test2);
 
         test1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"message",Toast.LENGTH_SHORT).show();
 
-                //Intent intent = new Intent(this,???.class);
-                //startActivity(intent);
+
+            }
+        });
+
+        test2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PicassoActivity.class);
+                startActivity(intent);
 
             }
         });
