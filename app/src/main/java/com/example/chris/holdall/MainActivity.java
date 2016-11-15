40c1f8c84e21f859a6307ba66a3a9f85878615bd
ChrisTouze <admin@lr-services.fr>
2016-11-15 14:41:26 +0100

    ADD button
    toast when clic button

--- app/src/main/java/com/example/chris/holdall/MainActivity.java
+++ app/src/main/java/com/example/chris/holdall/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.chris.holdall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button)findViewById(R.id.button);
-        TextView textView = (TextView)findViewById(R.id.my_text);
-        textView.setText("Bonjour");
+        Button test1 = (Button) findViewById(R.id.test1);
+
+        test1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"message",Toast.LENGTH_SHORT).show();
+
+                //Intent intent = new Intent(this,???.class);
+                //startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
